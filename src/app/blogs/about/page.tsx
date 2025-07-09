4d31274ac32792e050f3a7af8652f8b57d314239
Your Name <you@example.com>
2025-07-09 14:26:57 +0500

    Fix: removed use client from root page.tsx

--- src/app/blogs/about/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/about/page.tsx
-export default function AboutPage() {
-  return (
-    <div>
-      <h1>About TechNovaVista</h1>
-      <p>This site offers quality tech and SEO articles.</p>
-    </div>
-  );
-}
